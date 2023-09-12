64dfe890821603f76af97cea913be2af6388adb6
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-09-12 22:46:22 +0100

    added animations

--- src/components/models/Fish.jsx
+++ src/components/models/Fish.jsx
@@ -23,42 +23,12 @@ export function Fish({ hovered, ...props }) {
         <group name="CharacterArmature">
           <primitive object={nodes.Root} />
           <group name="Fish">
-            <skinnedMesh
-              name="Sphere026"
-              geometry={nodes.Sphere026.geometry}
-              material={materials.Fish_Main}
-              skeleton={nodes.Sphere026.skeleton}
-            />
-            <skinnedMesh
-              name="Sphere026_1"
-              geometry={nodes.Sphere026_1.geometry}
-              material={materials.Fish_Secondary}
-              skeleton={nodes.Sphere026_1.skeleton}
-            />
-            <skinnedMesh
-              name="Sphere026_2"
-              geometry={nodes.Sphere026_2.geometry}
-              material={materials.Eye_Black}
-              skeleton={nodes.Sphere026_2.skeleton}
-            />
-            <skinnedMesh
-              name="Sphere026_3"
-              geometry={nodes.Sphere026_3.geometry}
-              material={materials.Eye_White}
-              skeleton={nodes.Sphere026_3.skeleton}
-            />
-            <skinnedMesh
-              name="Sphere026_4"
-              geometry={nodes.Sphere026_4.geometry}
-              material={materials.Mouth}
-              skeleton={nodes.Sphere026_4.skeleton}
-            />
-            <skinnedMesh
-              name="Sphere026_5"
-              geometry={nodes.Sphere026_5.geometry}
-              material={materials.Fish_Flaps}
-              skeleton={nodes.Sphere026_5.skeleton}
-            />
+            <skinnedMesh name="Sphere026" geometry={nodes.Sphere026.geometry} material={materials.Fish_Main} skeleton={nodes.Sphere026.skeleton} />
+            <skinnedMesh name="Sphere026_1" geometry={nodes.Sphere026_1.geometry} material={materials.Fish_Secondary} skeleton={nodes.Sphere026_1.skeleton} />
+            <skinnedMesh name="Sphere026_2" geometry={nodes.Sphere026_2.geometry} material={materials.Eye_Black} skeleton={nodes.Sphere026_2.skeleton} />
+            <skinnedMesh name="Sphere026_3" geometry={nodes.Sphere026_3.geometry} material={materials.Eye_White} skeleton={nodes.Sphere026_3.skeleton} />
+            <skinnedMesh name="Sphere026_4" geometry={nodes.Sphere026_4.geometry} material={materials.Mouth} skeleton={nodes.Sphere026_4.skeleton} />
+            <skinnedMesh name="Sphere026_5" geometry={nodes.Sphere026_5.geometry} material={materials.Fish_Flaps} skeleton={nodes.Sphere026_5.skeleton} />
           </group>
         </group>
       </group>
